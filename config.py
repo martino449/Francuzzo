02c7c9caa306e175bf7acbe989f4d4b9a65ec63b
Martin449 <76888581+martino449@users.noreply.github.com>
2024-07-29 15:45:26 +0200

    adding config.py

---
+++ config.py
@@ -0,0 +1,26 @@
+destinations = {
+    "Documents": [
+        ".pdf",
+        ".docx",
+        ".txt"
+    ],
+    "Images": [
+        ".jpg",
+        ".jpeg",
+        ".png",
+        ".gif"
+    ],
+    "Videos": [
+        ".mp4",
+        ".mov",
+        ".avi"
+    ],
+    "zip": [
+        ".zip"
+    ]
+}
+
+patterns = {
+    "2014": "2014_Files",
+    "2015": "2015_Files"
+}
