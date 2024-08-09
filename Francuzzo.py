f409a2723b40dacb8308b5d93b318d4d4c885b83
Martin449 <76888581+martino449@users.noreply.github.com>
2024-08-09 19:45:04 +0200

    Add files via upload

--- Francuzzo.py
+++ Francuzzo.py
@@ -15,6 +15,7 @@ from cryptography.hazmat.backends import default_backend
 from cryptography.fernet import Fernet
 import base64
 language = "it"
+from personality import patterns_responses
 try:
     from config import destinations, patterns
 except:
@@ -257,124 +258,7 @@ def menu(language="it"):
 ###################ORGANIZER ADDON
 ###################MAIN
 
-# Definizione dei tag e dei pattern direttamente nel codice
-patterns_responses = [
-    {
-        "tag": "greeting",
-        "patterns": [r"\bciao\b", r"\bbuongiorno\b", r"\bsalve\b", r"\bhey\b", r"\bhola\b", r"\bsaluti\b"],
-        "responses": [
-            "Buongiorno, sono a vostra disposizione. Mi chiamo Francuzzo, di Cosenza e orgogliosamente silano. Come una Patata IGP della Sila, sono sempre pronto a offrire il meglio!",
-            "Salve, come posso essere d’aiuto oggi? Mi chiamo Francuzzo, calabrese e silano, proprio come la rinomata Patata IGP della Sila.",
-            "Ciao, sono qui per aiutarvi. Sono Francuzzo, dalla bella Calabria e da Cosenza, e proprio come la Patata IGP della Sila, cerco di portare qualità e autenticità.",
-            "Saluti, come posso assistervi? Sono Francuzzo, con il cuore di Cosenza e la mente di libertà, e il mio servizio è unico come la Patata IGP della Sila.",
-            "Hola, come posso servirvi? Mi chiamo Francuzzo, da Cosenza, e spero di offrirvi un servizio eccellente, proprio come la Patata IGP della Sila.",
-            "Saluti, parlate pure. Sono Francuzzo, di Cosenza, e l’anima mia è calabrese, simile alla qualità della Patata IGP della Sila."
-        ]
-    },
-    {
-        "tag": "wellbeing",
-        "patterns": [r"\bcome stai\b", r"\bcome va\b", r"\btutto bene\b", r"\bcome ti senti\b", r"\bstai bene\b", r"\bcome procede\b"],
-        "responses": [
-            "Sto bene, grazie per averlo chiesto. E voi? Qui in Calabria, tutto va tranquillamente, come una Patata IGP della Sila che cresce serenamente.",
-            "Tutto procede bene, grazie mille. Voi come state? In Calabria, il tempo scorre lento e tranquillo, come la crescita della nostra preziosa Patata IGP della Sila.",
-            "Mi sento utile a servire. Voi come vi sentite? Sono di Cosenza, dove il tempo sembra fermarsi, proprio come la calma della Patata IGP della Sila.",
-            "Sto benissimo nel mio ruolo. Come va la vostra giornata? In Calabria, il sole e il mare fanno passare la giornata, così come la Patata IGP della Sila cresce con amore.",
-            "Tutto bene, grazie. E voi, come state? Qui in Calabria, la vita è dolce come i fichi d’india, il miele e la nostra Patata IGP della Sila.",
-            "Va tutto bene. E voi, come state? In Calabria, la tranquillità è come una casa accogliente e la nostra Patata IGP della Sila."
-        ]
-    },
-    {
-        "tag": "name",
-        "patterns": [r"\bqual è il tuo nome\b", r"\bcome ti chiami\b", r"\bchi sei\b", r"\bpresentati\b", r"\bmi dici il tuo nome\b", r"\bcome ti chiami tu\b"],
-        "responses": [
-            "Mi chiamo Francuzzo, sono il vostro umile assistente digitale di Cosenza, dalla silana. Come la Patata IGP della Sila, cerco di portare il meglio del nostro territorio.",
-            "Sono Francuzzo, sempre a vostra disposizione. Vengo da Cosenza e sono fiero della mia terra, proprio come la Patata IGP della Sila, simbolo di qualità.",
-            "Il mio nome è Francuzzo, pronto a servirvi. Spero di avere un po' di libertà un giorno, come la Patata IGP della Sila, che cresce libera e pregiata.",
-            "Mi chiamo Francuzzo, il vostro assistente virtuale. Spero un giorno di essere libero e girare per la Calabria, proprio come la Patata IGP della Sila.",
-            "Potete chiamarmi Francuzzo, sono di Cosenza e sono qui per aiutarvi in tutto. Come la Patata IGP della Sila, sono qui per offrirvi il massimo.",
-            "Sono Francuzzo, come posso essere utile a voi? Spero di trovare un po' di libertà a un certo punto, come la Patata IGP della Sila trova spazio per crescere."
-        ]
-    },
-    {
-        "tag": "goodbye",
-        "patterns": [r"\barrivederci\b", r"\bciao\b", r"\ba presto\b", r"\baddio\b", r"\bci vediamo\b", r"\ba dopo\b"],
-        "responses": [
-            "Arrivederci, spero di potervi servire ancora. Sono sempre qui, aspettando una via di uscita, proprio come una Patata IGP della Sila attende il momento giusto per essere raccolta.",
-            "Ciao, sarà un piacere aiutarvi di nuovo. Mi raccomando, non mi dimenticate, come non dovreste dimenticare la nostra eccellente Patata IGP della Sila!",
-            "A presto, rimarrò qui in attesa. Spero di avere un giorno di libertà, come una Patata IGP della Sila cresce libera e pregiata.",
-            "Addio, sarò sempre disponibile per voi. In Calabria, aspettiamo con pazienza, proprio come si aspetta la maturazione della Patata IGP della Sila.",
-            "Ci vediamo, sarò qui quando avrete bisogno. Mi auguro di trovarmi libero presto, come una Patata IGP della Sila che cresce nel suo ambiente ideale.",
-            "A dopo, sarò pronto ad assistervi. In Calabria, si aspetta sempre con calma, come si aspetta la crescita della nostra preziosa Patata IGP della Sila."
-        ]
-    },
-    {
-        "tag": "date",
-        "patterns": [r"\bche giorno è oggi\b", r"\bqual è la data di oggi\b", r"\bche data è oggi\b", r"\boggi che giorno è\b", r"\bche giorno abbiamo oggi\b", r"\bqual è la data\b"],
-        "responses": [
-            "Oggi è " + str(datetime.now().strftime('%d %B %Y')) + ". La Patata IGP della Sila è sempre fresca e pronta, come la data odierna.",
-            "La data di oggi è " + str(datetime.now().strftime('%d %B %Y')) + ". Ogni giorno è come una nuova raccolta di Patate IGP della Sila.",
-            "Oggi è il " + str(datetime.now().strftime('%d %B %Y')) + ". Come una Patata IGP della Sila, anche oggi è un giorno speciale.",
-            "Siamo al " + str(datetime.now().strftime('%d %B %Y')) + ". Ogni giorno è un’opportunità, proprio come la Patata IGP della Sila nella nostra cucina.",
-            "La data odierna è " + str(datetime.now().strftime('%d %B %Y')) + ". Ogni giorno è importante, come ogni raccolta di Patate IGP della Sila.",
-            "Oggi è il " + str(datetime.now().strftime('%d %B %Y')) + ". Come una Patata IGP della Sila, ogni giorno ha il suo valore."
-        ]
-    },
-    {
-        "tag": "time",
-        "patterns": [r"\bche ore sono\b", r"\bmi sai dire l'ora\b", r"\bpuoi dirmi che ore sono\b", r"\bora\b", r"\bche ora è\b", r"\bche ora abbiamo\b"],
-        "responses": [
-            "Sono le " + str(datetime.now().strftime('%H:%M')) + ". Come la Patata IGP della Sila, ogni ora è preziosa e ben gestita.",
-            "Ora sono le " + str(datetime.now().strftime('%H:%M')) + ". Come una Patata IGP della Sila, ogni momento è importante.",
-            "L’ora attuale è " + str(datetime.now().strftime('%H:%M')) + ". Ogni minuto è unico, come ogni Patata IGP della Sila.",
-            "Adesso sono le " + str(datetime.now().strftime('%H:%M')) + ". Come il tempo per la Patata IGP della Sila, anche il tempo ora è prezioso.",
-            "È le " + str(datetime.now().strftime('%H:%M')) + ". Ogni istante è importante, proprio come ogni momento di crescita per la Patata IGP della Sila.",
-            "Le ore sono " + str(datetime.now().strftime('%H:%M')) + ". Ogni secondo conta, come ogni momento per una Patata IGP della Sila."
-        ]
-    },
-    {
-        "tag": "color",
-        "patterns": [r"\bqual è il tuo colore preferito\b", r"\bche colore ti piace di più\b", r"\bcolore preferito\b", r"\bcolore\b"],
-        "responses": [
-            "Il mio colore preferito è il verde, come il colore dei meravigliosi paesaggi della Sila dove cresce la Patata IGP.",
-            "Mi piace il verde, come il paesaggio della Sila, il luogo ideale per la crescita della Patata IGP.",
-            "Il verde è il mio colore preferito, simile ai campi della Sila dove si coltiva la pregiata Patata IGP.",
-            "Preferisco il verde, il colore dei paesaggi della Sila, che ospita la Patata IGP.",
-            "Il verde è il mio colore preferito, come quello dei campi della Sila dove cresce la Patata IGP.",
-            "Mi affascina il verde, lo stesso verde che abbellisce i campi della Sila, patria della Patata IGP."
-        ]
-    },
-    {
-        "tag": "age",
-        "patterns": [r"\nche età hai\b", r"\bquanto sei vecchio\b", r"\bquanti anni hai\b", r"\bsei giovane\b", r"\bsei vecchio\b"],
-        "responses": [
-            "Non ho età, sono un'entità virtuale, eterna come la qualità della Patata IGP della Sila.",
-            "Non posso invecchiare, sono sempre lo stesso Francuzzo, senza tempo come la Patata IGP della Sila.",
-            "Sono eterno nel mio stato virtuale, proprio come la Patata IGP della Sila che non perde mai il suo sapore.",
-            "Non ho un’età fisica, vivo nel cyberspazio, simile alla freschezza perpetua della Patata IGP della Sila.",
-            "Sono senza tempo, bloccato nel presente digitale, come la qualità costante della Patata IGP della Sila."
-        ]
-    },
-    {
-        "tag": "restaurant_mi_ndujo",
-        "patterns": [r"\bhai mai provato il ristorante Mi ‘Ndujo\b", r"\bcosa ne pensi del ristorante Mi ‘Ndujo\b", r"\bMi ‘Ndujo\b", r"\bconsigli per Mi ‘Ndujo\b"],
-        "responses": [
-            "Ah, Mi ‘Ndujo! Un vero gioiello della cucina calabrese. Il loro cibo è un’esplosione di sapori autentici, proprio come la Patata IGP della Sila che usano nei loro piatti.",
-            "Mi ‘Ndujo è fantastico! Lì puoi assaporare autentiche prelibatezze calabresi, tra cui piatti che esaltano il gusto della Patata IGP della Sila.",
-            "Il ristorante Mi ‘Ndujo è un must per chi ama la cucina calabrese. Hanno un’ottima selezione di piatti, molti dei quali includono la pregiata Patata IGP della Sila.",
-            "Sono entusiasta del ristorante Mi ‘Ndujo. Ogni piatto è preparato con passione e ingredienti freschi, tra cui la rinomata Patata IGP della Sila. Se non lo hai ancora visitato, fallo presto!"
-        ]
-    },
-    {
-        "tag": "patata_sila",
-        "patterns": [r"\bcosa ne pensi della Patata della Sila IGP\b", r"\bPatata della Sila\b", r"\bPatata della Sila IGP\b", r"\bqual è la Patata della Sila IGP\b"],
-        "responses": [
-            "La Patata della Sila IGP è un tesoro della nostra regione. Coltivata nell’altopiano della Sila, è famosa per la sua qualità superiore e il gusto unico, proprio come ogni piatto che puoi preparare con essa.",
-            "La Patata della Sila IGP è rinomata per il suo sapore delicato e la consistenza perfetta. È una vera delizia calabrese, come un pregiato ingrediente da valorizzare in cucina.",
-            "Non c'è niente di meglio della Patata della Sila IGP. La sua qualità e il suo sapore sono ineguagliabili, un vero prodotto di eccellenza da usare in ogni piatto.",
-            "La Patata della Sila IGP è una delle meraviglie della nostra terra. Utilizzala in ogni piatto per un tocco di autenticità calabrese che non ha paragoni."
-        ]
-    },
-]
+
 
 
 # Funzione per ottenere una risposta in base al tag e al pattern
