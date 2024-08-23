b12999cffe438f57002047296a20c7ce8c1a34b4
Martin449 <76888581+martino449@users.noreply.github.com>
2024-08-23 19:32:20 +0200

    Update Interpreter.py

--- Interpreter.py
+++ Interpreter.py
@@ -86,3 +86,20 @@ class M_interpreter:
                 self.replace_word(input_file, output_file)
 
 
+
+
+
+
+
+#Fine del codice
+# ---------------------------------------------
+# Copyright (c) 2024 Mario Pisano
+#
+# Questo programma è distribuito sotto la licenza EUPL, Versione 1.2 o – non appena 
+# saranno approvate dalla Commissione Europea – versioni successive della EUPL 
+# (la "Licenza");
+# Puoi usare, modificare e/o ridistribuire il programma sotto i termini della 
+# Licenza. 
+# 
+# Puoi trovare una copia della Licenza all'indirizzo:
+# https://joinup.ec.europa.eu/collection/eupl/eupl-text-eupl-12
